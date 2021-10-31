ee812e780cda1a23ef56ac5164ae765a159e9165
huargu <h.armagan.g@gmail.com>
2021-10-31 20:36:53 +0300

    primitive book menu

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BookMenu from './BookMenu'
 
 class Book extends Component {
   render() {
@@ -14,17 +15,7 @@ class Book extends Component {
                 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
             }}
           ></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+          <BookMenu />
         </div>
         <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
         <div className="book-authors">J.K. Rowling</div>
